88d7a3b4bd9767a1325a2617c593d8be0c80950a
Dmitry Shpakov <dmitryshpakov@Mumzas-MacBook-Pro.local>
2011-10-27 15:21:28 +0300

    Add: Fullscreen Window to respond to events in fullscreen
    Not sure this is really needed. Minimize and cmd+tab issue is not resolved yet.

---
+++ Sources/Internal/Platform/TemplateMacOS/FullscreenWindow.h
@@ -0,0 +1,17 @@
+//
+//  FullscreenWindow.h
+//  Framework
+//
+//  Created by Dmitry Shpakov on 10/25/11.
+//  Copyright 2011 DAVA Consulting. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface FullscreenWindow : NSWindow {
+@private
+    
+}
+
+@end
---
+++ Sources/Internal/Platform/TemplateMacOS/FullscreenWindow.m
@@ -0,0 +1,29 @@
+//
+//  FullscreenWindow.m
+//  Framework
+//
+//  Created by Dmitry Shpakov on 10/25/11.
+//  Copyright 2011 DAVA Consulting. All rights reserved.
+//
+
+#import "FullscreenWindow.h"
+
+
+@implementation FullscreenWindow
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
